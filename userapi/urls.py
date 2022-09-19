6496f081acfe4ccc4ce067538af2c49b438f50c8
Afsan Saeed <afsansaeed2017@gmail.com>
2022-09-19 23:26:04 +0600

    api url correction

--- userapi/urls.py
+++ userapi/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import UserDetailAPI,RegisterUserAPIView
 urlpatterns = [
-  path('details',UserDetailAPI.as_view()),
-  path('register',RegisterUserAPIView.as_view()),
+  path('v1/details',UserDetailAPI.as_view()),
+  path('v1/signup',RegisterUserAPIView.as_view()),
 ]
