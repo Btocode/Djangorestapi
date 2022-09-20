0a6abe8f5b3b8037952080332e6cc611691c0c74
Afsan Saeed <afsansaeed2017@gmail.com>
2022-09-20 11:24:36 +0600

    simple jwt and blacklisting

--- userapi/urls.py
+++ userapi/urls.py
@@ -1,7 +1,16 @@
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from django.urls import path
-from .views import UserDetailAPI,RegisterUserAPIView, LoginView
+from .views import UserDetailAPI,RegisterUserAPIView, LoginView, IndividualUserDetailAPI, LogoutView
+
 urlpatterns = [
   path('v1/details',UserDetailAPI.as_view()),
   path('v1/signup',RegisterUserAPIView.as_view()),
   path('v1/login',LoginView.as_view()),
+  path('v1/getuser',IndividualUserDetailAPI.as_view()),
+  path('v1/logout',LogoutView.as_view()),
+  path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+  path('v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- userapi/views.py
+++ userapi/views.py
@@ -6,8 +6,16 @@ from custom_user.models import CustomUser
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import generics
 from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+import jwt, datetime
+
 
 # Class based view to Get User Details using Token Authentication
+
+
 class UserDetailAPI(APIView):
   authentication_classes = (TokenAuthentication,)
   permission_classes = (AllowAny,)
@@ -36,6 +44,42 @@ class LoginView(APIView):
     if not user.check_password(password):
       raise AuthenticationFailed('Incorrect password!')
 
-    return Response({
-      "message": "Authentication Successful",
-    })
+    payload = {
+      'id': user.email,
+      'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=1),
+      'iat': datetime.datetime.utcnow()
+    }
+    token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+    response = Response()
+
+    response.set_cookie(key='jwt', value=token, httponly=True)
+    response.data = {
+      "jwt": token,
+    }
+
+    return response;
+
+class IndividualUserDetailAPI(APIView):
+  authentication_classes = (TokenAuthentication,)
+  permission_classes = (AllowAny,)
+  def get(self,request,*args,**kwargs):
+    token = request.COOKIES.get('jwt')
+    if not token:
+      raise AuthenticationFailed('User is not Authenticated')
+    try:
+      payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+    except jwt.ExpiredSignatureError:
+      raise AuthenticationFailed('Unauthenticated')
+    user = CustomUser.objects.filter(email = payload['id']).first()
+    serializer = UserSerializer(user)
+    return Response(serializer.data)
+
+class LogoutView(APIView):
+  def post(self, request, *args,**kwargs):
+    response = Response()
+    response.delete_cookie('jwt')
+    response.data = {
+      "message": "Successfully logged out"
+    }
+    return response
